03c5be214afc795b4a3e2b065845919dd602e1b2
franciscosuca <franciscosusana92@hotmail.com>
2025-01-28 08:48:19 +0100

    Fix to redirection for volunteering page.

--- app/volunteering/page.tsx
+++ app/volunteering/page.tsx
@@ -6,7 +6,7 @@ export default function Page() {
             <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
                 My Volunterring
             </h1>
-            <p className="mb-4"> {`Test`} </p>
+            <p className="mb-4"> {`(Place your volunteering experience in this page or section.)`} </p>
         </section>
     )
 }
