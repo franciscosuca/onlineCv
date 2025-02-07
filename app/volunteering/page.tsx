e07d2cd11762cac4e29a8d980050d40f3fe95148
franciscosuca <franciscosusana92@hotmail.com>
2025-02-07 09:04:23 +0100

    Update volunteering page with new component.

--- app/volunteering/page.tsx
+++ app/volunteering/page.tsx
@@ -1,6 +1,7 @@
+import { PostsDetails } from 'app/components/postsDetails';
 import { getPosts  } from 'app/utils/utils'
 
-let posts = getPosts('volunteering');
+let volunteeringPosts = getPosts('volunteering');
 
 export default function Page() {
     return (
@@ -8,17 +9,7 @@ export default function Page() {
             <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
                 My Volunteering
             </h1>
-            {posts.map((post, index) => (
-                <div key={index} className="flex flex-col space-y-2 mb-4">
-                    <p className="text-neutral-600 dark:text-neutral-400 w-[100px] tabular-nums">
-                    {post.metadata.date}</p>
-                    <p>{post.metadata.location}, {post.metadata.company}</p>
-                    <p className="italic-text">{post.metadata.jobTitle}</p>
-                    <article className="prose">
-                        {post.content}
-                    </article>
-                </div>
-            ))}
+            <PostsDetails posts={volunteeringPosts}/>
         </section>
     )
 }
