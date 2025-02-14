fe23b19db047c2a64df6f5720ad9b5696bab02f7
franciscosuca <franciscosusana92@hotmail.com>
2025-02-14 08:55:17 +0100

    Changes on pipe-config nad sitemap for fixing the pipeline.

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -4,7 +4,7 @@ export const baseUrl = 'https://portfolio-blog-starter.vercel.app'
 
 export default async function sitemap() {
     let posts = getPosts('workExperience').map((post) => ({
-      url: `${baseUrl}/blog/${post.slug}`,
+      url: `${baseUrl}/workExperience/${post.slug}`,
       lastModified: post.metadata.company,
     }))
     return [...posts]
