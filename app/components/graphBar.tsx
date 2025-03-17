d8ed9c19cb1b24eb7e60fc00ff9eac08977a35e5
franciscosuca <franciscosusana92@hotmail.com>
2025-03-17 08:37:16 +0100

    Add second graph with information about my technology stack.

---
+++ app/components/graphBar.tsx
@@ -0,0 +1,91 @@
+'use client';
+
+import { BarChart, Bar, XAxis, YAxis, ResponsiveContainer, Legend, CartesianGrid, Tooltip, ComposedChart } from 'recharts';
+
+interface skillData {
+    name: string;
+    totalYears: number;
+    yearsAtCompany: object;
+}
+
+interface GraphBarComponentProps {
+    skillData: skillData[];
+}
+
+// Define colors for the bars
+const colors = ['#3674B5', '#578FCA', '#A1E3F9', '#F9A1E3']
+
+// Get all unique companies names from the data
+function flatCompanies(skillData: skillData[]) {
+    return Array.from(new Set(skillData.flatMap(item => Object.keys(item.yearsAtCompany))));
+}
+
+export function GraphBarVer({ skillData }: GraphBarComponentProps) {
+
+    const companies = flatCompanies(skillData);
+    
+    return (
+        <ResponsiveContainer width="100%" height="100%">
+            <BarChart
+                width={600}
+                height={500}
+                data={skillData}
+                margin={{
+                    top: 20,
+                    right: 20,
+                    bottom: 20,
+                    left: 20,
+                }}
+                >
+                <XAxis dataKey="name"/>
+                <YAxis dataKey="totalYears"/>
+                <Legend />
+                <Tooltip />
+                {companies.map((company, index) => (
+                    <Bar
+                        key={`${company}-${index}`}
+                        dataKey={`yearsAtCompany.${company}`}
+                        stackId="1"
+                        fill={colors[index % colors.length]}
+                        name={company} />
+                ))}
+            </BarChart>
+        </ResponsiveContainer>
+    );
+}
+
+export function GraphBarHor({ skillData }: GraphBarComponentProps) {
+
+    const companies = flatCompanies(skillData);
+    
+    return (
+        <ResponsiveContainer width="100%" height="100%">
+            <ComposedChart
+                layout="vertical"
+                width={600}
+                height={400}
+                data={skillData}
+                margin={{
+                    top: 20,
+                    right: 20,
+                    bottom: 20,
+                    left: 20,
+                }}
+                >
+                <XAxis type="number"/>
+                <YAxis dataKey="name" type="category"/>
+                <Legend />
+                <Tooltip />
+                {companies.map((company, index) => (
+                    <Bar
+                        key={`${company}-${index}`}
+                        dataKey={`yearsAtCompany.${company}`}
+                        stackId="1"
+                        fill={colors[index % colors.length]}
+                        name={company}
+                    />
+                ))}
+            </ComposedChart>
+      </ResponsiveContainer>
+    );
+}
--- app/skills/page.tsx
+++ app/skills/page.tsx
@@ -1,5 +1,5 @@
 // import { getSkills } from "app/utils/utils";
-import {GraphBarComponent} from "app/components/graphBarComponent";
+import {GraphBarVer, GraphBarHor} from "app/components/graphBar";
 
 // Temporary data for the graph
 const testProgramingExperience = [
@@ -35,23 +35,92 @@ const testProgramingExperience = [
         }
     },
 ]
+// temporary technology data
 
-// TODO-1: define method to get data from skills for the graph above
+// Excel / PBI	2G/3G/4G/5G	AZ/GCP/AWS	React	Pipelines	Docker	Kubernetes	Ansible	Grafana	VPN
+// WIND	2	2				0				
+// Huawei	3	3				0				
+// Livello / HSRW			3	2	3	2	1	2	2	2
+// GEA	1		3	1	2	3			2	3
+
+const testTechStackExperience = [
+        {
+            name: '2G/3G/4G/5G',
+            totalYears: 6,
+            yearsAtCompany: {
+                'WIND': 2,
+                'Huawei': 4,
+            }
+        },
+        {
+            name: 'AZ/GCP/AWS',
+            totalYears: 6,
+            yearsAtCompany: {
+                'Livello/HSRW': 3,
+                'GEA': 3,
+            }
+        },
+        {
+            name: 'React',
+            totalYears: 3,
+            yearsAtCompany: {
+                'Livello/HSRW': 2,
+                'GEA': 1,
+            }
+        },
+        {
+            name: 'Pipelines',
+            totalYears: 5,
+            yearsAtCompany: {
+                'Livello/HSRW': 3,
+                'GEA': 2,
+            }
+        },
+        {
+            name: 'Docker',
+            totalYears: 3,
+            yearsAtCompany: {
+                'Livello/HSRW': 2,
+                'GEA': 1,
+            }
+        },
+        {
+            name: 'Kubernetes',
+            totalYears: 1,
+            yearsAtCompany: {
+                'Livello/HSRW': 1,
+            }
+        },
+        {
+            name: 'Ansible',
+            totalYears: 2,
+            yearsAtCompany: {
+                'Livello/HSRW': 2,
+            }
+        }
+]
+
+//* TODO-1: define method to get data from skills for the graph above
 // TODO-2: create seconds graph for the technologies
 // TODO-3: create a npm package for the graph component?
 
 export default function Page() {
     return (
         // TODO: Change the grid size for the cols below when using XL screen (greater than 1280 px)
-        <section className="grid xl:grid-cols-2 w-full gap-10 max-w-[1400px]">
+        <section className="grid xl:grid-cols-2 w-full gap-10 max-w-[1600px]">
             <h1  className="mb-8 text-2xl font-semibold tracking-tighter">Skills</h1>
             <p>In the graph below you can find the years of experience that I have collected per programming language on each company that I have work for.</p>
 
             <GridItem title="Programming Experience">
-                <GraphBarComponent skillData={testProgramingExperience} />
+                <GraphBarVer skillData={testProgramingExperience} />
             </GridItem>
+            <GridItem title="Technology Stack">
+                <GraphBarHor skillData={testTechStackExperience} />
+            </GridItem>
+
         </section>
     );
+    // TODO: Enable the code below when the getSkills method is defined
     {/* <ul>
         {getSkills().map((skill) => (
             <li key={skill}>{skill}</li>
@@ -61,8 +130,8 @@ export default function Page() {
 
 function GridItem({ title, children }) {
     return (
-        <div className="flex flex-col items-center justify-center p-4 border border-slate-900 bg-slate-900/1 rounded-xl h-[400px]">
-        <h3 className="text-2xl font-semibold text-white mb-4">{title}</h3>
+        <div className="flex flex-col items-center justify-center p-4 border border-slate-900 bg-slate-900/1 rounded-xl h-[400px] w-[600px]">
+        <h3 className="text-2xl font-semibold text-blue-500 mb-4">{title}</h3>
         {children}
       </div>
     );
