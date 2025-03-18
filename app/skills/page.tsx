fcac3e62597a652243da395a89e0ad4480b045b5
Francisco Susana <37332293+franciscosuca@users.noreply.github.com>
2025-03-18 08:55:56 +0100

    Feature/skill page (#15) - Enhances graphs for the skill page
    * Minor changes to the project for its first release.
    
    * Add skills parameter to the experience data, and display it on a separate page.
    
    * Test rechtarts for creating graphics for the skills.
    
    * Enable the graph as a component.
    
    * Add barchart to the skills page.
    
    * Enable stacked barpgrah-component.
    
    * Adapt the graph to work with the data from the skills page and add legend to the barGraph.
    
    * Enable website with the first graph for the skill page.
    
    * Update the project version and add some comments for the skill page.
    
    * Update packages.
    
    * Fix schema use for the skills.
    
    * Add await for data expected from workExperience.
    
    * Changes to fix the build of the project.
    
    * Update to Nextjs 15.2.2
    
    * Update project goals.
    
    * Add second graph with information about my technology stack.
    
    * Correct temporary data to sample the graphs and improve the style for them.
    
    * Add react-icons dependency to the project and enable a hamburger menu.
    
    * Update project plan.

--- app/skills/page.tsx
+++ app/skills/page.tsx
@@ -1,15 +1,8 @@
 // import { getSkills } from "app/utils/utils";
-import {GraphBarComponent} from "app/components/graphBarComponent";
+import {GraphBarVer, GraphBarHor} from "app/components/graphBar";
 
 // Temporary data for the graph
 const testProgramingExperience = [
-    {
-        name: 'Java',
-        totalYears: 1,
-        yearsAtCompany: {
-            'Livello/HSRW': 1,
-        }
-    },
     {
         name: 'C#',
         totalYears: 1,
@@ -35,23 +28,95 @@ const testProgramingExperience = [
         }
     },
 ]
-
-// TODO-1: define method to get data from skills for the graph above
-// TODO-2: create seconds graph for the technologies
-// TODO-3: create a npm package for the graph component?
+// temporary technology data
+const testTechStackExperience = [
+        {
+            name: 'AZ/GCP',
+            totalYears: 5,
+            yearsAtCompany: {
+                'GEA': 3,
+                'Livello/HSRW': 2,
+            }
+        },
+        {
+            name: '.NET',
+            totalYears: 3,
+            yearsAtCompany: {
+                'GEA': 3,
+            }
+        },
+        {
+            name: 'React/React Native',
+            totalYears: 3,
+            yearsAtCompany: {
+                'Livello/HSRW': 2,
+                'GEA': 1,
+            }
+        },
+        {
+            name: '2G/3G/4G/5G',
+            totalYears: 6,
+            yearsAtCompany: {
+                'Huawei': 3.5,
+                'WIND': 2,
+            }
+        },
+        {
+            name: 'GitLab/DevOps Pipelines',
+            totalYears: 5,
+            yearsAtCompany: {
+                'Livello/HSRW': 3,
+                'GEA': 2,
+            }
+        },
+        {
+            name: 'SQL/NoSQL',
+            totalYears: 4,
+            yearsAtCompany: {
+                'Livello/HSRW': 2,
+                'GEA': 2,
+            }
+        },
+        {
+            name: 'Docker',
+            totalYears: 4,
+            yearsAtCompany: {
+                'Livello/HSRW': 2,
+                'GEA': 2,
+            }
+        },
+        {
+            name: 'Kubernetes',
+            totalYears: 1,
+            yearsAtCompany: {
+                'Livello/HSRW': 1,
+            }
+        },
+        {
+            name: 'Ansible',
+            totalYears: 2,
+            yearsAtCompany: {
+                'Livello/HSRW': 2,
+            }
+        }
+]
 
 export default function Page() {
     return (
-        // TODO: Change the grid size for the cols below when using XL screen (greater than 1280 px)
-        <section className="grid xl:grid-cols-2 w-full gap-10 max-w-[1400px]">
+        <section className="grid xl:grid-cols-2 w-full gap-10 max-w-[1600px]">
             <h1  className="mb-8 text-2xl font-semibold tracking-tighter">Skills</h1>
             <p>In the graph below you can find the years of experience that I have collected per programming language on each company that I have work for.</p>
 
-            <GridItem title="Programming Experience">
-                <GraphBarComponent skillData={testProgramingExperience} />
+            <GridItem title="Main Programming Experience">
+                <GraphBarVer skillData={testProgramingExperience} />
             </GridItem>
+            <GridItem title="Main Technology Stack">
+                <GraphBarHor skillData={testTechStackExperience} />
+            </GridItem>
+
         </section>
     );
+    // TODO: Enable the code below when the getSkills method is defined
     {/* <ul>
         {getSkills().map((skill) => (
             <li key={skill}>{skill}</li>
@@ -61,8 +126,8 @@ export default function Page() {
 
 function GridItem({ title, children }) {
     return (
-        <div className="flex flex-col items-center justify-center p-4 border border-slate-900 bg-slate-900/1 rounded-xl h-[400px]">
-        <h3 className="text-2xl font-semibold text-white mb-4">{title}</h3>
+        <div className="flex flex-col items-center justify-center p-4 border border-slate-900 bg-slate-900/1 rounded-xl h-[400px] w-[700px]">
+        <h3 className="text-2xl font-semibold text-blue-500 mb-4">{title}</h3>
         {children}
       </div>
     );
