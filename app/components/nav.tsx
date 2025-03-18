6b962bd342e7541fca9c39e67f5a65ac8043b2db
franciscosuca <franciscosusana92@hotmail.com>
2025-03-18 08:48:42 +0100

    Add react-icons dependency to the project and enable a hamburger menu.

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -1,4 +1,8 @@
+'use client';
+
 import Link from 'next/link'
+import { useState } from 'react'
+import { FiMenu, FiX } from 'react-icons/fi'
 
 const navItems = {
   '/': {
@@ -19,13 +23,28 @@ const navItems = {
 }
 
 export function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen)
+  }
+
   return (
     <aside className="-ml-[8px] mb-16 tracking-tight">
       <div className="lg:sticky lg:top-20">
+        {/* HamburgerMenu */}
+        <div className='md:hidden'>
+          <button
+            onClick={toggleMenu}
+            className="text-2xl focus:outline-none"
+            aria-label="Toggle Menu">
+            {isMenuOpen ? <FiX/> : <FiMenu/>}
+          </button>
+        </div>
+        {/* NavMenu */}
         <nav
-          className="flex flex-row items-start relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
-          id="nav"
-        >
+          className={`${isMenuOpen ? 'block' : 'hidden'} md:flex flex-col md:flex-row items-start relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative`}
+          id="nav">
           <div className="flex flex-row space-x-0 pr-10">
             {Object.entries(navItems).map(([path, { name }]) => {
               return (
