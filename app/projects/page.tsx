3222d791f9c1632f6cd62caa20eb4204f13c4c02
Francisco Susana <37332293+franciscosuca@users.noreply.github.com>
2025-04-06 22:40:43 +0200

    fea: initiate the embededLinks component & re-arrange the order and content of the pages.

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,15 +1,16 @@
 import { PostList } from 'app/components/postsList'
 import { getPosts } from 'app/utils/utils'
+import { useEffect } from 'react'
 
 let projectPosts = getPosts('projects')
 
-export default function page() { 
+export default function page() {
     return (
         <section>
             <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
                 Projects
             </h1>
-            <PostList posts={projectPosts}/>
+            <PostList posts={projectPosts} />
         </section>
     )
 }
