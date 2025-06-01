4a50629bc51e360e5c896bdbdb7e30064ded0020
Francisco Susana <37332293+franciscosuca@users.noreply.github.com>
2025-06-01 20:39:12 +0200

    chore: adapt project experience page to use comosDB

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,16 +1,15 @@
 import { PostList } from 'app/components/postsList'
-import { getPosts } from 'app/utils/utils'
-import { useEffect } from 'react'
+import { Experience } from 'app/types/Experience';
+import { queryItems } from 'app/utils/cosmosDB'
 
-let projectPosts = getPosts('projects')
-
-export default function page() {
+export default async function page() {
+    let projecExperience = await queryItems<Experience>('research'); //TODO: change the partitionKey to the one used for projects in CosmosDB
     return (
         <section>
             <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
                 Projects
             </h1>
-            <PostList posts={projectPosts} />
+            <PostList posts={projecExperience} />
         </section>
     )
 }
