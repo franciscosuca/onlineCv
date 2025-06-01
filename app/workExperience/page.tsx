62fc2d41605ebb7dd0214023d539976c94320fad
Francisco Susana <37332293+franciscosuca@users.noreply.github.com>
2025-06-01 20:39:27 +0200

    chore: code refactory.

--- app/workExperience/page.tsx
+++ app/workExperience/page.tsx
@@ -1,16 +1,15 @@
 import { PostList } from "app/components/postsList"
-import { queryItems } from "app/utils/cosmosDB";
-import { Experience } from "app/types/Experience";
-
+import { queryItems } from "app/utils/cosmosDB"
+import { Experience } from "app/types/Experience"
 
 export default async function Page() {
-    let workExperience = await queryItems<Experience>("research");
-    return (
-        <section>
-            <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-                Work Experience
-            </h1>
-            <PostList posts={ workExperience } />
-        </section>
-    );
+  let workExperience = await queryItems<Experience>("research"); //TODO: change the partitionKey to the one used for projects in CosmosDB
+  return (
+    <section>
+      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
+        Work Experience
+      </h1>
+      <PostList posts={workExperience} />
+    </section>
+  );
 }
